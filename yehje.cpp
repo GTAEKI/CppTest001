1c86d49b0e508e4f1a6384a11d15bc79006cc517
배경택 <kyungtaek0204@gmail.com>
2023-06-04 22:54:32 +0900

    Practice committing with the command

--- yehje.cpp
+++ yehje.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
-#include <stdio.h> 
+#include <time.h> //시간
+#include <stdlib.h> //rand,srand
+#include <unistd.h> // <windows.h> 대체
 
-//유저에게 사이즈를 입력받아서 *로 정사각형 출력하기
 int main(){
-    int number,number2,number3;
-    printf("정사각형 공장입니다.\n");
-    printf("3 ~ 10사이 숫자를 입력해주세요:");
-    scanf("%d",&number);
-    if(number < 3){
-        printf("다시 시작하세요\n");
-        number = 0;
-    }else if(number > 10){
-        printf("다시 시작하세요\n");
-        number = 0;
-    }
+    setbuf(stdout, NULL); // 버퍼링 비활성화
 
-    number2 = number;
-    number3 = number;
+    int dice = 0;
+    srand(time(NULL));
 
-    while(1 <= number){
-        while(1<=number2){
-            printf("* ");
-            number2 -= 1;
-        }
-        if(number2 < 1){
-            number2 = number3;
-        }
-        printf("\n");
-        number -= 1;
+    printf("주사위의 숫자는 : ");
+
+    getchar();
+
+    
+
+    for(int count = 0; count < 3; count++){
+        sleep(1);
+        dice = rand()%6+1;
+        printf("%d ",dice);
+
     }
+    printf("\n");
 }
