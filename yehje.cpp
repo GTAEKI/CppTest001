2d088e27dc533ecec09b4690247867a5a08e87d4
배경택 <kyungtaek0204@gmail.com>
2023-06-05 19:36:58 +0900

    배열에 맞춰 별모양 움직이기

--- yehje.cpp
+++ yehje.cpp
@@ -1,18 +1,188 @@
-// 포인터 공부
+// 문제
+// hard 출력되는 0을 엔터하나 입력할때 마다 오른쪽으로 한칸씩 이동하게하기
+// very hard : 위의 프로그램을 수정해서 a로 0이 왼쪽, d로 0이 오른쪽 이동 가능하도록 만들기
 
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
 
-void AddOne(int localNumber1, int localNumber2);
+void hard();
+void hard2();
+void veryHard();
 
-int main(){
-    int mainNumber1 = 0;
-    int mainNumber2 = 0;
+int main()
+{
+    //hard();
+   //hard2();
+   veryHard();
 
-    AddOne(mainNumber1,mainNumber2);
-    printf("number의 상태 [1] = %d, [2] = %d \n\n", mainNumber1,mainNumber2);
 }
 
-void AddOne(int localNumber1, int localNumber2){
-    localNumber1 += 1;
-    localNumber2 += 1;
+void veryHard()
+{
+    char board[5] = {0,};
+    char key = '0';
+    
+    // {board를 초기화 하는 로직
+    for(int a = 0; a<5; a++)
+    {
+        for(int i = 0; i<5; i++)
+        {
+            board[i] = '*';
+            if(i==a)
+            {
+            board[i] = '0';
+            }
+        } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%c ",board[i]);
+        }
+
+        cin>>key;
+        
+        if(key == 'A' || key =='a')
+        {
+            a = a - 2;
+        }
+        
+        printf("\n\n");
+    }
+    
+}
+
+void hard2()
+{
+    char board[5] = {0,};
+    
+    // {board를 초기화 하는 로직
+    for(int a = 0; a<5; a++)
+    {
+        for(int i = 0; i<5; i++)
+        {
+            board[i] = '*';
+            if(i==a)
+            {
+            board[i] = '0';
+            }
+        } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+        for (int i = 0; i < 5; i++)
+        {
+            printf("%c ",board[i]);
+        }
+        getchar();
+        printf("\n\n");
+    }
+    
+}
+
+void hard()
+{
+    char board[5] = {0,};
+   int count = 1;
+
+   while(count < 6)
+    {
+        getchar();
+
+        // {board를 초기화 하는 로직
+        switch(count)
+        {
+          case 1: 
+            for(int i = 0; i<5; i++)
+            {
+                board[i] = '*';
+                if(i==0)
+                {
+                board[i] = '0';
+                }
+            } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%c ",board[i]);
+            }
+            break;
+            case 2: 
+            for(int i = 0; i<5; i++)
+            {
+                board[i] = '*';
+                if(i==1)
+                {
+                board[i] = '0';
+                }
+            } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%c ",board[i]);
+            }
+            break;
+            case 3: 
+            for(int i = 0; i<5; i++)
+            {
+                board[i] = '*';
+                if(i==2)
+                {
+                board[i] = '0';
+                }
+            } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%c ",board[i]);
+            }
+            break;
+            case 4: 
+            for(int i = 0; i<5; i++)
+            {
+                board[i] = '*';
+                if(i==3)
+                {
+                board[i] = '0';
+                }
+            } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%c ",board[i]);
+            }
+            break;
+            case 5: 
+            for(int i = 0; i<5; i++)
+            {
+                board[i] = '*';
+                if(i==4)
+                {
+                board[i] = '0';
+                }
+            } 
+            // board를 초기화 하는 로직}
+            
+            // {board를 출력하는 로직
+            for (int i = 0; i < 5; i++)
+            {
+                printf("%c ",board[i]);
+            }
+            break;
+            // board를 출력하는 로직}
+        }
+        count++;
+    }
+
+    printf("\n\n");
 }
